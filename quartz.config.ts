edf38cf9faff836f0f7cbfa8d6952b60de3142c3
junbeom-cho <dev.junbeom@gmail.com>
2026-03-17 22:08:04 +0900

    update config

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Home",
+    pageTitle: "Junbeom's Tech Blog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
