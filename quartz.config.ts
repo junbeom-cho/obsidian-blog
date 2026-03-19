8f571448951823c0a556ce96cd73190fbac0aac1
junbeom-cho <dev.junbeom@gmail.com>
2026-03-19 14:02:06 +0900

    fix Blog Title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Junbeom's Tech Blog",
+    pageTitle: "Second Brain",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
