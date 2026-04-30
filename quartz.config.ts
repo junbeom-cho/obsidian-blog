6d80869cb175702fa806e08cbba0276939285c0c
junbeom-cho <dev.junbeom@gmail.com>
2026-04-30 12:58:47 +0900

    Edit Conf

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "ko-KR",
     baseUrl: "obsidian.junbeom.work",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
