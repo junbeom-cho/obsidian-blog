47bc554736624294bc1d7e0c98efd578cc7c5233
junbeom-cho <dev.junbeom@gmail.com>
2026-04-30 12:58:37 +0900

    edit scripts

--- scripts/build-auto.sh
+++ scripts/build-auto.sh
@@ -1,9 +1,14 @@
 #!/bin/zsh
 
+echo ""
+
 echo "============================================="
 echo "빌드 시작 시간: $(date +"%Y-%m-%d %H:%M:%S")"
 echo "Quartz 자동 빌드 중..."
 echo "============================================="
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "NVM_DIR/nvm.sh" ] && \. "NVM_DIR/nvm.sh"
 
 # 빌드
 cd /home/junbeom/quartz-blog/
@@ -12,3 +17,5 @@ npx quartz build --directory "/home/junbeom/obsidian-vault/"
 echo "=============================================="
 echo "빌드 완료 시간: $(date +"%Y-%m-%d %H:%M:%S")"
 echo "=============================================="
+
+echo ""
