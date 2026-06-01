0221de0803b874b5a0d9f9b95f561f3f48a308fc
junbeom-cho <dev.junbeom@gmail.com>
2026-06-01 11:17:20 +0900

    Edit HostName

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "ko-KR",
-    baseUrl: "obsidian.junbeom.work",
+    baseUrl: "wiki.junbeom.work",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
