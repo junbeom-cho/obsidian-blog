179c4482e0782350ff80aaab467f02045203379a
junbeom-cho <dev.junbeom@gmail.com>
2026-07-22 17:29:21 +0900

    chore: Remove Sync in Build Script

--- ubuntu-build.sh
+++ ubuntu-build.sh
@@ -1,3 +1,2 @@
 #!/bin/zsh
-npx quartz sync
 npx quartz build -d /home/techbara/obsidian-vault
--- win-build.bat
+++ win-build.bat
@@ -1,3 +1,2 @@
 @echo off
-npx quartz sync
 npx quartz build --serve --directory "C:\Users\user\projects\obsidian-vault" --port 8081
